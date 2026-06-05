0c823786f02d159417d853f9931025910bfc175b
gudenamrutha <gudenamrutha@gmail.com>
2026-06-05 05:00:46 +0200

    environment variables added

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios  from "axios";
 
-const API_URL = "https://job-application-tracker-ngpc.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL : API_URL
